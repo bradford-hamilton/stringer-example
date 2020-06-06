d66ea114932bc1f6e315144738968477751d4f87
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2020-06-06 09:13:47 -0600

    Initial commit
    Have never used stringer until now - super cool - wanted
    to try it out and have an example.

---
+++ README.md
@@ -0,0 +1,3 @@
+### Quick example from std lib around using stringer
+
+go get -u golang.org/x/tools/cmd/stringer
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bradford-hamilton/stringer-example/pkg/painkiller"
+)
+
+func main() {
+	fmt.Println(painkiller.Aspirin) // Aspirin
+}
